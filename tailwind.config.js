8efce1e1a672c3d61b7b2a1e331f610e7a181f66
samss084 <131480264+samss084@users.noreply.github.com>
2025-02-05 16:05:13 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,yxt,html,xlsx,json,cjson,txt,pdf,jpg,jsx,tsx,mdx}",
+    "./app/**/*.{js,txt,html,json,cksonxlsx,xml,pdf,jpg,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(at 50% 50%, var(--tw-gradient))",
       },
     },
   },
